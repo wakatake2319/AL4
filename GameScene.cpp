c335b89ad2746624f7c43597c578a1c6cb5b3b4c
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-05-09 10:36:04 +0900

    AL3_01_01課題

---
+++ GameScene.cpp
@@ -0,0 +1,11 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+// 初期化
+void GameScene::Initialize() {}
+
+// 更新
+void GameScene::Update() {}
+
+// 更新
+void GameScene::Draw() {}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
 #include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE2C_18_タケウチ_ハルカ_AL3");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画処理をここに記述
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+	}
+
+	// ゲームシーンの開放
+	delete gameScene;
+	// nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
