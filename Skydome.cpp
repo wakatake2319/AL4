232be4783e12a14061c84e5674f19e55bf2f4b2c
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-05-16 13:08:05 +0900

    途中

--- Skydome.cpp
+++ Skydome.cpp
@@ -1,19 +1,18 @@
 #include "Skydome.h"
 
 // 初期化
-void Skydome::Initialize() {}
+void Skydome::Initialize() {
+
+
+}
 
 // 更新
 void Skydome::Update() {}
 
 // 描画
 void Skydome::Draw() {
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
-	Model::PreDraw(dxCommon->GetCommandList());
 
 	// 3Dモデル描画
 	model_->Draw(worldTransform_, camera_);
 
-	Model::PostDraw();
 }
